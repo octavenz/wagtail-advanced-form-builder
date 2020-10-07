9c8fdd80bf2880f36813c8bc6c9af61abbf4fc01
Richard Blake <richieblake@gmail.com>
2020-10-07 23:52:16 +1300

    Ensure Page content panels are displayed in default FormPage

--- wagtail_advanced_form_builder/models/form_page.py
+++ wagtail_advanced_form_builder/models/form_page.py
@@ -5,5 +5,7 @@ from wagtail_advanced_form_builder.models.abstract_advanced_form import Abstract
 
 class FormPage(AbstractAdvancedForm):
 
+    content_panels = Page.content_panels + AbstractAdvancedForm.content_panels
+
     settings_panels = Page.settings_panels + AbstractAdvancedForm.settings_panels
 
