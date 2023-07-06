cb2ced9a94d7e97596bc0564305157f77ca9d5f9
stephencockerill <stephen@typecode.com>
2023-07-06 16:11:19 -0400

    add optional salesforce config fields to all field blocks

--- wagtail_advanced_form_builder/blocks/fields/base_field_block.py
+++ wagtail_advanced_form_builder/blocks/fields/base_field_block.py
@@ -8,23 +8,20 @@ from .conditional_block import ConditionalBlock
 
 
 class BaseFieldBlock(blocks.StructBlock):
-
     label = blocks.CharBlock(
-        max_length=255,
-        required=True,
-        help_text='The label of the form field.'
+        max_length=255, required=True, help_text="The label of the form field."
     )
 
     required = blocks.BooleanBlock(
         default=False,
         required=False,
-        help_text='Tick this box to make this a required field.'
+        help_text="Tick this box to make this a required field.",
     )
 
     display_checkbox_label = blocks.BooleanBlock(
         default=False,
         required=False,
-        help_text='Do you want the checkbox label to display? If not you should populate help_text.'
+        help_text="Do you want the checkbox label to display? If not you should populate help_text.",
     )
 
     choices = blocks.ListBlock(
@@ -39,14 +36,21 @@ class BaseFieldBlock(blocks.StructBlock):
     )
 
     max_length = blocks.IntegerBlock(
-        required=False,
-        help_text='Set a maximum length for this field. e.g. 100'
+        required=False, help_text="Set a maximum length for this field. e.g. 100"
     )
 
     default_value = blocks.CharBlock(
+        max_length=255, required=False, help_text="Set a default value for this field."
+    )
+    salesforce_object_name = blocks.CharBlock(
         max_length=255,
         required=False,
-        help_text='Set a default value for this field.'
+        help_text="The Salesforce object name that the form field value will be integrated to.",
+    )
+    salesforce_api_field_name = blocks.CharBlock(
+        max_length=255,
+        required=False,
+        help_text="The Salesforce API field name that the form field value will be integrated to.",
     )
 
     # placeholder = blocks.CharBlock(
@@ -58,7 +62,7 @@ class BaseFieldBlock(blocks.StructBlock):
     help_text = blocks.CharBlock(
         max_length=255,
         required=False,
-        help_text='Text to assist the user in populating this field.'
+        help_text="Text to assist the user in populating this field.",
     )
 
     html = RichTextBlock(
@@ -66,15 +70,15 @@ class BaseFieldBlock(blocks.StructBlock):
     )
 
     display_side_by_side = blocks.BooleanBlock(
-        help_text='Display these items side by side?',
+        help_text="Display these items side by side?",
         required=False,
     )
 
     rules = ConditionalBlock(
         required=False,
-        help_text='Add conditional rules to show or hide fields depending on the value of other fields in the form.'
+        help_text="Add conditional rules to show or hide fields depending on the value of other fields in the form.",
     )
 
     @property
     def media(self):
-        return forms.Media(js=[static('wagtail_advanced_form_builder/js/admin.js')])
+        return forms.Media(js=[static("wagtail_advanced_form_builder/js/admin.js")])
